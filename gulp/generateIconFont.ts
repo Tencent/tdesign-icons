da00e6d6a69c707f5a9aba44565f67a835fd8f73
jiyangwei <845612246@qq.com>
2021-04-22 22:02:18 +0800

    fix: 生成json文件

--- gulp/generateIconFont.ts
+++ gulp/generateIconFont.ts
@@ -1,6 +1,8 @@
 import { dest, src } from 'gulp';
+import concat from 'gulp-concat';
 import iconfont from 'gulp-iconfont';
 import iconfontCss from 'gulp-iconfont-css';
+import { createTransformStream } from './transform';
 const runTimestamp = Math.round(Date.now() / 1000);
 export const generateIconFont = ({
   iconGlob,
@@ -10,6 +12,7 @@ export const generateIconFont = ({
   iconGlob: string;
   targetDir: string;
   fontCssConfig: Object;
+  fontJsonConfig: Object;
 }) =>
   function generateIconFont() {
     return src([iconGlob])
@@ -22,5 +25,34 @@ export const generateIconFont = ({
           timestamp: runTimestamp, // recommended to get consistent builds when watching files
         }),
       )
+      .on('codepoints', (results) => {
+        console.error(results);
+      })
       .pipe(dest(targetDir));
   };
+
+export const generateIconFontJson = ({ iconGlob, targetDir }: { iconGlob: string; targetDir: string }) =>
+  function generateIconFont() {
+    return src([iconGlob])
+      .pipe(useItemJsonTemplate())
+      .pipe(concat('index.json'))
+      .pipe(useJsonTemplate())
+      .pipe(dest(targetDir));
+  };
+
+function useItemJsonTemplate() {
+  function getItem(content: string, name: string) {
+    return `{"name": "${name}","svgCode": '${content}',"codepoint": "\\${name.split('-')[0]}"},`;
+  }
+  return createTransformStream((_, { stem: name }) => getItem(_, name));
+}
+
+function useJsonTemplate() {
+  function getContainer(content: string) {
+    return `
+    {"iconName":"t","icons":[
+      ${content}
+    ]}`;
+  }
+  return createTransformStream((content) => getContainer(content));
+}

950b1b53d77ff5f5eb0b4784245572fce771f5e7
jiyangwei <845612246@qq.com>
2021-04-23 15:27:32 +0800

    fix: 解析unicode码

--- gulp/generateIconFont.ts
+++ gulp/generateIconFont.ts
@@ -4,6 +4,11 @@ import iconfont from 'gulp-iconfont';
 import iconfontCss from 'gulp-iconfont-css';
 import { createTransformStream } from './transform';
 const runTimestamp = Math.round(Date.now() / 1000);
+const svgMap: any = {};
+interface GLYPHS {
+  name: string;
+  unicode: string;
+}
 export const generateIconFont = ({
   iconGlob,
   targetDir,
@@ -12,7 +17,6 @@ export const generateIconFont = ({
   iconGlob: string;
   targetDir: string;
   fontCssConfig: Object;
-  fontJsonConfig: Object;
 }) =>
   function generateIconFont() {
     return src([iconGlob])
@@ -25,8 +29,10 @@ export const generateIconFont = ({
           timestamp: runTimestamp, // recommended to get consistent builds when watching files
         }),
       )
-      .on('codepoints', (results) => {
-        console.error(results);
+      .on('glyphs', (glyphs: GLYPHS[]) => {
+        glyphs.forEach((item) => {
+          svgMap[item.name] = item.unicode;
+        });
       })
       .pipe(dest(targetDir));
   };
@@ -42,7 +48,7 @@ export const generateIconFontJson = ({ iconGlob, targetDir }: { iconGlob: string
 
 function useItemJsonTemplate() {
   function getItem(content: string, name: string) {
-    return `{"name": "${name}","svgCode": '${content}',"codepoint": "\\${name.split('-')[0]}"},`;
+    return `{"name": "${name}","svgCode": '${content}',"codepoint": "\\\\${escape(svgMap[name]).replace('%u', '')}"},`;
   }
   return createTransformStream((_, { stem: name }) => getItem(_, name));
 }
@@ -50,9 +56,7 @@ function useItemJsonTemplate() {
 function useJsonTemplate() {
   function getContainer(content: string) {
     return `
-    {"iconName":"t","icons":[
-      ${content}
-    ]}`;
+    {"iconName":"t","icons":[${content}]}`;
   }
   return createTransformStream((content) => getContainer(content));
 }

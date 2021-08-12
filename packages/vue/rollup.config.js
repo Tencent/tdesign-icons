ada8bd4d357672d942a5cafc2586a7759e33006b
carsonlei <carsonlei>
2021-08-12 17:24:55 +0800

    feat(vue/dist): 完善vue umd 格式打包

--- packages/vue/rollup.config.js
+++ packages/vue/rollup.config.js
@@ -1,10 +1,12 @@
 import vue from 'rollup-plugin-vue';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 
 const defaults = {
   compilerOptions: {
-    declaration: true,
+    declaration: false,
     module: 'es2015',
   },
 };
@@ -13,43 +15,17 @@ export default [
   {
     input: 'src/index.ts',
     output: {
-      format: 'cjs',
-      file: 'lib/index.js',
-    },
-    external: ['vue'],
-    plugins: [
-      typescript({
-        tsconfigDefaults: defaults,
-      }),
-      vue({ template: { optimizeSSR: true } }),
-    ],
-  },
-  {
-    input: 'src/index.ts',
-    output: {
-      format: 'esm',
-      file: 'esm/index.js',
-    },
-    external: ['vue'],
-    plugins: [
-      typescript({
-        tsconfigDefaults: defaults,
-      }),
-      vue(),
-    ],
-  },
-  {
-    input: 'src/index.ts',
-    output: {
       format: 'umd',
       file: 'dist/index.js',
-      name: 'library',
+      name: 'tdesignIconsVue',
       sourcemap: true,
     },
     external: ['vue'],
     plugins: [
+      nodeResolve(),
+      commonjs(),
       typescript({
-        tsconfigDefaults: defaults,
+        tsconfigOverride: defaults,
       }),
       vue({ template: { optimizeSSR: true } }),
     ],
@@ -59,13 +35,15 @@ export default [
     output: {
       format: 'umd',
       file: 'dist/index.min.js',
-      name: 'library',
+      name: 'tdesignIconsVue',
       sourcemap: true,
     },
     external: ['vue'],
     plugins: [
+      nodeResolve(),
+      commonjs(),
       typescript({
-        tsconfigDefaults: defaults,
+        tsconfigOverride: defaults,
       }),
       vue({ template: { optimizeSSR: true } }),
       terser(),

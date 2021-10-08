238fd7a62be9f05d759015c8ac53c125e5d8b646
uyarn <uyarnchen@gmail.com>
2021-10-08 20:23:35 +0800

    feat: 修复svgsprite使用问题

--- packages/svg-sprite/gulp/use-svg-sprite-template.ts
+++ packages/svg-sprite/gulp/use-svg-sprite-template.ts
@@ -4,6 +4,6 @@ import fs from 'fs';
 const template = fs.readFileSync(path.resolve(__dirname, 'template/index.js'), 'utf-8');
 
 export function getSvgSpriteJSFile(): string {
-  const svgContent = fs.readFileSync(path.resolve(__dirname, '../dist/css/svg/sprite.css.svg'), 'utf-8');
+  const svgContent = fs.readFileSync(path.resolve(__dirname, '../dist/symbol/svg/sprite.symbol.svg'), 'utf-8');
   return template.replace(/\$SVGSPRITE/g, svgContent);
 }

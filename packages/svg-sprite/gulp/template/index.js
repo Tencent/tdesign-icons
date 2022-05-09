80d42ae078b911f407efec4282ab8c8b36bc92b4
Uyarn <uyarnchen@gmail.com>
2022-05-09 14:52:18 +0800

    chore: support IE use icon source

--- packages/svg-sprite/gulp/template/index.js
+++ packages/svg-sprite/gulp/template/index.js
@@ -1,10 +1,10 @@
 // This file is generated automatically by `useSvgSpriteTemplate.ts`. DO NOT EDIT IT.
 (function () {
-  const svgCode = '$SVGSPRITE';
+  var svgCode = '$SVGSPRITE';
   if (document.body) {
     document.body.insertAdjacentHTML('afterbegin', svgCode);
   } else {
-    document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('DOMContentLoaded', function () {
       document.body.insertAdjacentHTML('afterbegin', svgCode);
     });
   }

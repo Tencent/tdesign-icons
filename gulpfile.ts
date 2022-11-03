55213fa78468ceefe48527264874cc33c61145d8
Uyarn <uyarnchen@gmail.com>
2022-11-03 19:24:57 +0800

    chore: remove useless gulp

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,14 +1,9 @@
 import { parallel, series } from 'gulp';
 
 import { reactTask } from './packages/react/gulp';
-import { angularTask } from './packages/angular/gulp';
-import { svgTask } from './packages/svg/gulp';
-import { vueTask } from './packages/vue/gulp';
-import { vueNextTask } from './packages/vue-next/gulp';
 
 import { svgSpriteTask } from './packages/svg-sprite/gulp';
 import { iconFontTask } from './packages/icon-font/gulp';
-
 import { iconViewTask } from './packages/view/gulp';
 
 const source: string[] = ['svg/*.svg'];
@@ -16,10 +11,6 @@ const source: string[] = ['svg/*.svg'];
 export default series(
   parallel(
     reactTask(source),
-    vueTask(source),
-    vueNextTask(source),
-    svgTask(source),
-    angularTask(source),
     svgSpriteTask(),
     iconFontTask(source),
     iconViewTask(source),

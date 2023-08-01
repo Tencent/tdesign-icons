4dd8cc79355d8d3404052f9f9b83b1054847e7c1
Uyarn <uyarnchen@gmail.com>
2023-08-01 15:22:26 +0800

    feat: upgrade cdn source

--- packages/vue/src/iconfont/icon.tsx
+++ packages/vue/src/iconfont/icon.tsx
@@ -8,7 +8,7 @@ import '../style/css';
 
 const { classPrefix } = ConfigContext;
 
-const CDN_ICONFONT_URL = 'https://tdesign.gtimg.com/icon/0.1.4/fonts/index.css';
+const CDN_ICONFONT_URL = 'https://tdesign.gtimg.com/icon/0.2.0/fonts/index.css';
 
 export const IconFont = Vue.extend({
   name: 'IconFont',

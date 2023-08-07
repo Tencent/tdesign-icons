88527fbe0c05b13a07acbfa0f6f8f4d0c178700e
Uyarn <uyarnchen@gmail.com>
2023-08-07 14:59:19 +0800

    feat: new icon view UI

--- packages/view/src/IconView.vue
+++ packages/view/src/IconView.vue
@@ -1,17 +1,24 @@
 <template>
-  <t-space style="width: 100%">
-    <t-space>
-      <t-radio-group default-value="filled" variant="default-filled">
+  <t-space :style="{ width: '100%' }" direction="vertical">
+    <div :style="{ width: '100%', display: 'flex' }">
+      <t-radio-group v-model="currentType" variant="default-filled">
         <t-radio-button value="filled">{{ lang.types.filled }}</t-radio-button>
         <t-radio-button value="outline">{{
           lang.types.outline
         }}</t-radio-button>
       </t-radio-group>
-      <t-input :placeholder="lang.search" />
-    </t-space>
-    <t-space>
-      
-    </t-space>
+      <t-input
+        :placeholder="lang.search"
+        :style="{ flex: 1, marginLeft: '16px' }"
+      />
+    </div>
+    <div>
+      <t-tabs v-model="selectTab">
+        <t-tab-panel v-for="tab in tabs" :label="tab.labelCN" :value="tab.labelEn">
+          <t-icon v-for="(icon) in tab.icons" :name="icon" :key="icon"></t-icon>
+        </t-tab-panel>
+      </t-tabs>
+    </div>
   </t-space>
 </template>
 <script setup>
@@ -20,18 +27,37 @@ import {
   RadioButton as TRadioButton,
   Input as TInput,
   Space as TSpace,
+  Tabs as TTabs,
+  TabPanel as TTabPanel,
 } from "tdesign-vue";
-import { onMounted, ref } from "vue";
+import { Icon as TIcon } from 'tdesign-icons-vue'
+import { onMounted, ref, computed, watch } from "vue";
 import zhCN from "./i18n/zh-CN";
 import enUS from "./i18n/en-US";
+import { categories } from "./category";
 
-const lang = ref();
+const lang = ref(zhCN);
+const currentType = ref("filled");
+const selectTab = ref("");
+
+const tabs = computed(() => {
+  return categories[currentType.value] || {};
+});
+
 onMounted(() => {
   const isEn = window.location.pathname.endsWith("en");
   lang.value = isEn ? enUS : zhCN;
 });
+
+watch(
+  () => currentType.value,
+  () => {
+    const tabCategories = Object.keys(tabs.value);
+    selectTab.value = tabs.value[tabCategories[0]].labelEn;
+  }
+);
 </script>
-<style></style>
-<style lang="less" scoped>
+<style scoped>
 @import "../node_modules/tdesign-vue/dist/tdesign.css";
+@import "./vars.css";
 </style>

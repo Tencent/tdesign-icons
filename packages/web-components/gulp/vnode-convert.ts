36986c9b7af6b026c8dc54f446d1e2bd20d32a20
En Yang Du <377153400@qq.com>
2024-07-31 18:04:42 +0800

    fix(wc): fix clip-rule and fill-rule  (#100)
    * feat(web-components): web components icon complete🎉
    
    * feat(web-components): web components icon complete🎉
    
    * feat(icon): add tdesign-icons-web-components
    
    * feat(icon): add tdesign-icons-web-components
    
    * feat(icon): add tdesign-icons-web-components
    
    * chore: fix typo
    
    * feat(icon): add tdesign-icons-web-components
    
    * feat(web-components): fix README.md
    
    * fix(web-components): 修复svg fill-rule 属性问题
    
    ---------
    
    Co-authored-by: wū yāng <uyarnchen@gmail.com>

--- packages/web-components/gulp/vnode-convert.ts
+++ packages/web-components/gulp/vnode-convert.ts
@@ -1,9 +1,21 @@
 import type { VNode } from 'omi';
 import type { IconElement } from '../../../gulp/svg-info-check';
 
+const convertAttributes = (attrs) => Object.keys(attrs).reduce((acc, key) => {
+  const value = attrs[key];
+  if (key === 'fillRule') {
+    acc['fill-rule'] = value;
+  } else if (key === 'clipRule') {
+    acc['clip-rule'] = value;
+  } else {
+    acc[key] = value;
+  }
+  return acc;
+}, {});
+
 export default function vnodeConvert(el: IconElement) {
   const omiNode:VNode = {
-    attributes: el.attrs,
+    attributes: convertAttributes(el.attrs),
     nodeName: el.tag,
     children: [],
   };

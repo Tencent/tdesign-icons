d95834f1710e82d8ed5cebc7e9d1a9ce77c1231d
Uyarn <uyarnchen@gmail.com>
2024-08-28 22:12:26 +0800

    fix(vue-next): fix umd usage

--- packages/vue-next/src/index.ts
+++ packages/vue-next/src/index.ts
@@ -1,5 +1,20 @@
+import { App } from 'vue';
+import withInstall from './utils/with-install';
+import * as icons from './icons';
+
+export function install(app: App): void {
+  Object.keys(icons).forEach((key) => {
+    // @ts-ignore
+    const icon = withInstall(icons[key]);
+    app.use(icon);
+  });
+}
+
 export * from './icons';
 export * from './manifest';
 export * from './iconfont/index';
 export * from './svg-sprite/index';
 export * from './global-config';
+export default {
+  install,
+};
---
+++ packages/vue-next/src/utils/with-install.ts
@@ -0,0 +1,18 @@
+import {
+  App, Plugin, Component,
+} from 'vue';
+
+function withInstall<T>(
+  comp: T,
+  alias?: string,
+): T & Plugin {
+  const componentPlugin = comp as T & Component & Plugin;
+
+  componentPlugin.install = (app: App, name?: string) => {
+    app.component(alias || name || componentPlugin.name, comp);
+  };
+
+  return componentPlugin as T & Plugin;
+}
+
+export default withInstall;

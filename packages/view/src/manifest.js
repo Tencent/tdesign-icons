73c47341490f93b2e8d711919f5afa2b83225255
黎伟杰 <674416404@qq.com>
2026-02-09 08:59:12 +0800

    docs(view): update no-result icon search keyword

--- packages/view/src/manifest.js
+++ packages/view/src/manifest.js
@@ -8141,7 +8141,7 @@ export const manifest = {
         },
         {
           name: 'no-result',
-          keywords: ['无结果'],
+          keywords: ['无结果', '无数据', '空', 'empty'],
         },
         {
           name: 'notification',

e4c728500be8885dd15c2c29dc5d1847406ee2a9
阿菜 Cai <jimmyrss1102@gmail.com>
2026-06-02 21:26:39 +0800

    chore: 移除时间戳

--- gulp/generate-icon-font.ts
+++ gulp/generate-icon-font.ts
@@ -24,7 +24,6 @@ const webComponentsCss = `@font-face {
   font-style: normal;
 }`;
 
-const runTimestamp = Math.round(Date.now() / 1000);
 const svgMap: any = {};
 interface GLYPHS {
   name: string;
@@ -50,7 +49,7 @@ export const generateIconFont = ({
           fontName: 't', // required
           prependUnicode: true, // recommended option
           formats: ['svg', 'ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
-          timestamp: runTimestamp, // recommended to get consistent builds when watching files
+          // 不填 timestamp，使用 gulp-iconfont 默认值
           normalize: true,
           fontHeight: 1024,
         }),

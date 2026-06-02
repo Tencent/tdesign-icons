83229e265d843aa4e329800ceba72225e59bc412
驴割肠 <noob-coder@qq.com>
2026-06-02 12:36:44 +0800

    Remove type annotations for IconData and allIconsMap

--- packages/flutter/tool/generate.dart
+++ packages/flutter/tool/generate.dart
@@ -80,7 +80,6 @@ void generate(List<IconModel> icons) {
                       ])
                       ..static = true
                       ..modifier = FieldModifier.constant
-                      ..type = refer('IconData')
                       ..name = icon.name
                       ..assignment = refer('IconData')
                           .newInstance(
@@ -97,7 +96,6 @@ void generate(List<IconModel> icons) {
                     ..docs.addAll(['/// "$fontFamily" all icons. '])
                     ..static = true
                     ..modifier = FieldModifier.constant
-                    ..type = refer('<String, IconData>')
                     ..name = 'allIconsMap'
                     ..assignment = literalMap(
                       {

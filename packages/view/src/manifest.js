62ee6a6aa6169c3c1c77be2fb92091cc83a73e1b
lulu <124988288+RADWIMPS426@users.noreply.github.com>
2026-07-10 14:22:54 +0800

    fix: fix two icon naming errors (#251)
    * feat: add new icon
    
    * chore: remove duplicated manifest content
    
    ---------
    
    Co-authored-by: Uyarn <uyarnchen@gmail.com>

--- packages/view/src/manifest.js
+++ packages/view/src/manifest.js
@@ -5761,16 +5761,12 @@ export const manifest = {
           keywords: ['点赞', '点踩'],
         },
         {
-          name: 'thumb-up',
-          keywords: ['点赞', '竖起大拇指', '赞'],
-        },
-        {
           name: 'thumb-up-1',
           keywords: ['点赞', '竖起大拇指'],
         },
         {
           name: 'thumb-up-2',
-          keywords: ['点赞', '翘起大拇指'],
+          keywords: ['点赞', '翘起大拇指', '赞', '好评'],
         },
         {
           name: 'undertake',

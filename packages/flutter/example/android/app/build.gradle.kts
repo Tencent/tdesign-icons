504fda89cb53520f75139ca2b9f5710688026934
阿菜 Cai <jimmyrss1102@gmail.com>
2026-08-04 11:20:59 +0800

    fix(flutter): align example Android toolchain

--- packages/flutter/example/android/app/build.gradle.kts
+++ packages/flutter/example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.tdesign.tdesign_icons_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17

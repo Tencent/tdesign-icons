417e06257bf790fe7d28546782808eba2503c666
liweijie <674416404@qq.com>
2026-08-05 23:18:45 +0800

    build(web-components): 添加私有方法和属性插件支持
    添加 @babel/plugin-transform-private-methods 和 @babel/plugin-transform-private-property-in-object 插件以支持私有字段转换。

--- packages/web-components/rolldown.config.mjs
+++ packages/web-components/rolldown.config.mjs
@@ -39,6 +39,8 @@ const babelPlugin = babel({
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
     ['@babel/plugin-transform-react-jsx', { pragma: 'h', pragmaFrag: 'h.f' }],
     '@babel/plugin-transform-runtime',
   ],

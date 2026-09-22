969eddee4ec2ba8eacb142ae6ee71c2c8ceb76d4
黎伟杰 <674416404@qq.com>
2026-09-22 09:16:37 +0800

    refactor(view): 使用自定义元素替换 Vue 应用挂载

--- packages/view/src/main.js
+++ packages/view/src/main.js
@@ -1,8 +1,1 @@
-import { createApp } from 'vue';
-
-import 'tdesign-vue-next/dist/tdesign.min.css';
-import IconView from './icon-view.vue';
-
-const app = createApp(IconView);
-
-app.mount('#app');
+import './build-entry';
